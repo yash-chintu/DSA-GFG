74c218a43e3d8c8fce1278646809bfb8ff0ebff3
unknown <krishnayaswanth4@gmail.com>
2021-09-27 21:24:17 +0530

    local

--- Linked List/Traverse.cpp
+++ Linked List/Traverse.cpp
@@ -4,11 +4,12 @@ class Solution
     void display(Node *head)
     {
       //your code goes here
-      
+
       while(head){
           cout<<head->data<<" ";
           head=head->next;
       }
+      
       return;
     }
 };
